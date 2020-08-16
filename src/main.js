55a0cd978bde0f351feaf8999ab07b3cfaeeeafe
sharjeel <leo8545@gmail.com>
2020-08-16 17:06:25 +0500

    Created noteAdded event to add to notes

--- src/main.js
+++ src/main.js
@@ -15,12 +15,37 @@ if (addNoteForm) {
 		if (!title) {
 			alert("title can not be empty");
 		} else {
-			notes.push({
+			const newNote = {
 				title,
 				content,
 				tags,
 				createdAt: new Date().toJSON(),
-			});
+			};
+			document.dispatchEvent(
+				new CustomEvent("noteAdded", { detail: { newNote, prevNotes: notes } })
+			);
+			notes.push(newNote);
+
+			// Empty the form fields
+			addNoteForm.querySelector("#title").value = "";
+			addNoteForm.querySelector("#content").value = "";
+			addNoteForm.querySelector("#tags").value = "";
+		}
+	});
+
+	// When notes is added
+	document.addEventListener("noteAdded", (e) => {
+		const list = document.querySelector("#notes-list");
+		// Add notes to sidebar
+		if (e.detail.newNote) {
+			const li = document.createElement("li");
+			const title = document.createElement("div");
+			const date = document.createElement("div");
+			title.textContent = e.detail.newNote.title;
+			date.textContent = e.detail.newNote.createdAt;
+			li.append(title);
+			li.append(date);
+			list.append(li);
 		}
 	});
 }
