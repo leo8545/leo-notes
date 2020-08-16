4f1e4dae52dfaf76ac28047c27f51cbe2daca826
sharjeel <leo8545@gmail.com>
2020-08-16 16:42:05 +0500

    Created notes array to store all notes added

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 const addNoteForm = document.querySelector("#add-note-form");
 if (addNoteForm) {
+	// To store all notes
+	let notes = [];
+
+	// On add a note form submit
 	addNoteForm.addEventListener("submit", (e) => {
 		e.preventDefault();
-		
+
+		// Form data
+		const title = addNoteForm.querySelector("#title").value;
+		const content = addNoteForm.querySelector("#content").value;
+		const tags = addNoteForm.querySelector("#tags").value;
+
+		if (!title) {
+			alert("title can not be empty");
+		} else {
+			notes.push({
+				title,
+				content,
+				tags,
+				createdAt: new Date().toJSON(),
+			});
+		}
 	});
 }
