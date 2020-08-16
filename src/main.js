b455c732fcd5dc4c11a432ea2c58a32a891d5de9
sharjeel <leo8545@gmail.com>
2020-08-16 20:01:05 +0500

    Added edit button & event

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 const addNoteForm = document.querySelector("#add-note-form");
+// To store all notes
+let notes = [];
 if (addNoteForm) {
-	// To store all notes
-	let notes = [];
-
 	// On add a note form submit
 	addNoteForm.addEventListener("submit", (e) => {
 		e.preventDefault();
@@ -20,6 +19,7 @@ if (addNoteForm) {
 				title,
 				content,
 				tags,
+				id: Date.now(),
 				createdAt: `${String(date.getDate()).padStart(2, "0")}.${String(
 					date.getMonth() + 1
 				).padStart(
@@ -45,16 +45,58 @@ if (addNoteForm) {
 		// Add notes to sidebar
 		if (e.detail.newNote) {
 			const li = document.createElement("li");
+
+			const head = document.createElement("header");
+			const foot = document.createElement("footer");
+			const article = document.createElement("article");
+
 			const title = document.createElement("div");
 			const date = document.createElement("div");
+			const content = document.createElement("div");
+			const btnEdit = document.createElement("button");
+
 			title.classList.add("title");
 			date.classList.add("date");
+			content.classList.add("content");
+			btnEdit.classList.add("editNote");
+			btnEdit.setAttribute("data-note", e.detail.newNote.id);
+
 			title.textContent = e.detail.newNote.title;
 			date.textContent = e.detail.newNote.createdAt;
-			li.append(title);
-			li.append(date);
+			content.textContent = e.detail.newNote.content;
+			btnEdit.textContent = "Edit";
+
+			foot.append(date);
+			head.append(title);
+			head.append(btnEdit);
+			article.append(content);
+
+			li.append(head);
+			li.append(article);
+			li.append(foot);
+
 			list.append(li);
+
+			li.id = e.detail.newNote.id;
 			li.classList.add("note-item", "magictime", "boingInUp");
+			document.dispatchEvent(
+				new CustomEvent("noteListItemAdded", { detail: { li, notes } })
+			);
 		}
 	});
 }
+
+document.addEventListener("noteListItemAdded", (event) => {
+	document.querySelectorAll(".editNote").forEach((btn) => {
+		btn.addEventListener("click", (e) => {
+			e.preventDefault();
+			const note = notes.filter(
+				(note) => note.id === parseInt(btn.parentNode.parentNode.id)
+			);
+			if (note) {
+				addNoteForm.querySelector("#title").value = note[0].title;
+				addNoteForm.querySelector("#content").value = note[0].content;
+			}
+		});
+	});
+});
